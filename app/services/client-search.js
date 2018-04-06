5aac40571343eb8b948d061184100014bea1e003
Aaron Chambers <achambers@gmail.com>
2018-04-06 23:20:36 +0100

    Create client-search component

---
+++ app/services/client-search.js
@@ -0,0 +1,60 @@
+import Service from '@ember/service';
+import { inject as service } from '@ember/service';
+import fetch from 'fetch';
+import config from 'phorest-vouchers/config/environment';
+
+export default Service.extend({
+  session: service(),
+
+  async byEmail(term) {
+    if (!term) { return; }
+
+    let options = this._requestOptions();
+    let url = this._requestUrl();
+    url = `${url}?email=${term}`;
+
+    let json = await this._fetch(url, options);
+
+    return this._handleResponse(json);
+  },
+
+  async byPhone(term) {
+    if (!term) { return; }
+
+    let options = this._requestOptions();
+    let url = this._requestUrl();
+    url = `${url}?phone=${term}`;
+
+    let json = await this._fetch(url, options);
+
+    return this._handleResponse(json);
+  },
+
+  _fetch(url, options) {
+    return fetch(url, options).then(resp => resp.json());
+  },
+
+  _requestOptions() {
+    let username = 'global/cloud@apiexamples.com';
+    let password = 'VMlRo/eh+Xd8M~l';
+    let authToken = btoa(`${username}:${password}`);
+
+    let options = {
+      headers: {
+        'Authorization': `Basic ${authToken}`
+      }
+    };
+
+    return options;
+  },
+
+  _requestUrl() {
+    let tenantId = this.get('session.currentTenant.id');
+
+    return `${config.apiHost}/third-party-api-server/api/business/${tenantId}/client`;
+  },
+
+  _handleResponse(json) {
+    return (json._embedded && json._embedded.clients) || [];
+  }
+});
