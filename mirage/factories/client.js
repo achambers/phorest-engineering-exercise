09edd3919334fde4a2506ff2a889a9d9f8a4a808
Aaron Chambers <achambers@gmail.com>
2018-04-08 20:22:34 +0100

    Fix a foobar in the mirage factory

--- mirage/factories/client.js
+++ mirage/factories/client.js
@@ -1,18 +1,18 @@
 import { Factory, faker } from 'ember-cli-mirage';
 
 export default Factory.extend({
-  clientId : 'ZyOj1R5bO2wvT2BPGYazhQ',
-  version : 1,
-  firstName : faker.name.firstName(),
-  lastName : faker.name.lastName(),
-  mobile : '888888855599222',
-  email : 'techtest@phorest.com',
-  clientSince : '2017-08-01T16:01:07.000Z',
-  notes : '0',
-  smsMarketingConsent : true,
-  emailMarketingConsent : true,
-  smsReminderConsent : true,
-  emailReminderConsent : true,
-  creatingBranchId : 'SE-J0emUgQnya14mOGdQSw',
-  archived : false
+  clientId: 'ZyOj1R5bO2wvT2BPGYazhQ',
+  version: 1,
+  firstName: () => faker.name.firstName(),
+  lastName: () => faker.name.lastName(),
+  mobile: '888888855599222',
+  email: 'techtest@phorest.com',
+  clientSince: '2017-08-01T16:01:07.000Z',
+  notes: '0',
+  smsMarketingConsent: true,
+  emailMarketingConsent: true,
+  smsReminderConsent: true,
+  emailReminderConsent: true,
+  creatingBranchId: 'SE-J0emUgQnya14mOGdQSw',
+  archived: false
 });
