97860be39e77443c77924ab905f85e1665562570
Aaron Chambers <achambers@gmail.com>
2018-04-09 14:59:33 +0100

    Basic version of client search using ember-power-select

--- app/components/vouchers-page/component.js
+++ app/components/vouchers-page/component.js
@@ -1,4 +1,23 @@
 import Component from '@ember/component';
+import { task, timeout } from 'ember-concurrency';
+import fetch from 'fetch';
 
 export default Component.extend({
+  searchClients: task(function* (term) {
+    yield timeout(600);
+
+    let tenantId = 'eTC3QY5W3p_HmGHezKfxJw';
+    let username = 'global/cloud@apiexamples.com';
+    let password = 'VMlRo/eh+Xd8M~l';
+    let authToken = btoa(`${username}:${password}`);
+    let url = `http://api-gateway-dev.phorest.com/third-party-api-server/api/business/${tenantId}/client?phone=${term}`;
+
+    let options = {
+      headers: {
+        'Authorization': `Basic ${authToken}`
+      }
+    };
+
+    return fetch(url, options).then((resp) => resp.json()).then((json) => json._embedded.clients);
+  }).restartable()
 });
