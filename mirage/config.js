765065d8614439d8f7631114f5514bfd550944f0
Aaron Chambers <achambers@gmail.com>
2018-04-10 17:26:07 +0100

    Mirage endpoint for creating vouchers

--- mirage/config.js
+++ mirage/config.js
@@ -8,8 +8,8 @@ export default function() {
     Note: these only affect routes defined *after* them!
   */
 
-   this.urlPrefix = 'http://api-gateway-dev.phorest.com';    // make this `http://localhost:8080`, for example, if your API is on a different server
-   this.namespace = 'third-party-api-server/api';    // make this `/api`, for example, if your API is namespaced
+  this.urlPrefix = 'http://api-gateway-dev.phorest.com';    // make this `http://localhost:8080`, for example, if your API is on a different server
+  this.namespace = 'third-party-api-server/api';    // make this `/api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
   /*
@@ -48,5 +48,14 @@ export default function() {
         }
       };
     }
+  });
+
+  this.post('business/:id/voucher', (schema, req) => {
+    var params = JSON.parse(req.requestBody);
+
+    return Object.assign(params, {
+      voucherId: 'akLKuiOtpkQfl_QZZfDt0g',
+      serialNumber: '10772'
+    })
   });
 }
